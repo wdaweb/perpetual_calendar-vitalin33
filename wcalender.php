0fcf0579f48a1a65e0977d179ba698fa59737417
vita_lin <vitalinchia@gmail.com>
2020-11-09 23:29:48 +0800

    追加前端設計-part1:圖片變化

--- wcalender.php
+++ wcalender.php
@@ -14,6 +14,7 @@
         @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Indie+Flower&family=Itim&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
 
 
         body{
@@ -54,7 +55,7 @@
         }
 
         .container{
-            margin-top:150px;
+            margin-top:50px;
             position:relative;
             display:flex;
             justify-content:space-between;
@@ -62,14 +63,51 @@
             border-radius:60px;
             padding:30px;
             background-color:#FEFEFE;
-
+            box-shadow:9px 11px 11px 3px black;
+            /* opacity:0.97; */
         }
 
         .box1{
             margin:0;
             border:1px solid transparent;
             border-radius:60px;
-            background-image:url('https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80');
+            background: url(
+                '<?php $a=array('https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                                'https://images.unsplash.com/photo-1509697452888-7cdcf2c98813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1503756234508-e32369269deb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+                                'https://images.unsplash.com/photo-1516452391242-15957b3bb26a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=627&q=80',
+                                'https://images.unsplash.com/photo-1479064312651-24524fb55c0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1485841938031-1bf81239b815?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=614&q=80',
+                                'https://images.unsplash.com/photo-1472739841375-d0ea9f0cb6a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60  ',
+                                'https://images.unsplash.com/photo-1517937151269-ac35e3ca6bd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1604852465282-bfe1e37a858d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80',
+                                'https://images.unsplash.com/photo-1604633791817-ddf1e6d2ff2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1541749504779-0e668281c3a6?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1604251036494-6568bd88db5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1573602929979-69ba82d94dd0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1580133318324-f2f76d987dd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1590083767559-68115ac987bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1583750956438-0ee5b4c62569?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1603798590145-ba522bde439e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1603548746365-0c7a1583d826?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+                                'https://images.unsplash.com/photo-1603268891126-edb67deb74ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
+                                'https://images.unsplash.com/photo-1520853538280-53740d2bd110?ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+                                'https://images.unsplash.com/photo-1603532654387-fc0a4555e8ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=647&q=80',
+                                'https://images.unsplash.com/photo-1602042986607-00418605d4f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1602661162654-75b5fa3b445d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=623&q=80',
+                                'https://images.unsplash.com/photo-1555188220-90cc2cad3537?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1600080972464-8e5f35f63d08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1604539645988-9d87b2c21d53?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1604407232062-5cdaa10a2328?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=627&q=80',
+                                'https://images.unsplash.com/photo-1604178450035-38e6a54ffa0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1603783033071-551d35a9a29f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1602562603289-8b52f21ef638?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1581224463294-908316338239?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+                                'https://images.unsplash.com/photo-1604564035384-bd3dfda9d369?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
+                                'https://images.unsplash.com/photo-1594913785111-0c95bf86bd65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'                       
+
+                                ); 
+            echo $a[array_rand($a)];?>');
             background-repeat:no-repeat;
             background-size:cover;
             width:49%;
@@ -78,15 +116,14 @@
         }
 
         .box2{
-            margin:0;           
+            margin:0px;           
+            padding-top:50px;
             border:1px solid transparent;
             width:49%;
             height:750px;
-
         }
 
         .weekend{
-
             color:red;
         }
 
@@ -96,7 +133,6 @@
 
 
         .pyear{
-
             font-family: 'Indie Flower', cursive;
             font-size:3rem;
             line-height:36px;
@@ -110,23 +146,113 @@
             font-weight:600;
         }
         .pmonth{
-
             font-family: 'Caveat', cursive;
             font-size:2.5rem;
             line-height:36px;
         }
+
+        .hi_1,.hi_2{
+            display:flex;
+            position:absolute;
+            width:100px;
+            height:70px;
+            background:linear-gradient(68deg, rgba(38,48,48,1) 0%, rgba(207,154,101,1) 28%, rgba(156,93,31,1) 49%, rgba(136,101,66,1) 69%, rgba(55,29,5,1) 100%);
+            border:1px solid transparent;
+            border-radius:0 0 20% 20%;
+            z-index:7;
+            box-shadow:4px 3px 6px 0px black;
+        }
+
+        .hi_1{
+            left:600px;
+            top:0;
+        }
+        .hi_2{
+            
+            right:600px;
+            top:0;
+        }
+
+        .m_1,.m_2{
+            display:flex;
+            position:absolute;
+            width:30px;
+            height:10px;
+            border:1px solid transparent;
+            border-radius:50%;
+            z-index:8;
+            background:linear-gradient(68deg, rgba(255,255,255,1) 0%, rgba(154,154,154,1) 28%, rgba(249,249,249,1) 49%, rgba(147,147,147,1) 69%, rgba(111,111,111,1) 100%);
+            box-shadow:4px 3px 6px 0px black;
+        }
+
+        .m_1{
+            left:635px;
+            top:53px;
+        }
+        .m_2{
+            right:635px;
+            top:53px;
+        }
+
+        .under_word{
+            display:flex;
+            position:absolute;
+            text-align:center;
+            margin-top:100px;
+            color:#333;
+            font-family: 'Pacifico', cursive;
+            font-size:15rem;
+            transform:rotate(350deg);
+            overflow:visible;
+            opacity:0.8;
+        }
+
+        .coffee_cup{
+
+            display:flex;
+            position:absolute;
+            right:80px;
+            z-index:10;
+            bottom:10px;
+            transform:scale(1.2,1.2);
+            overflow:visible;
+
+        }
+
+        /* .smoke{
+            display:flex;
+            position:absolute;
+            right:300px;
+            z-index:11;
+            bottom:400px;
+            width: 30px; height: 100px;
+            background-image: linear-gradient(68deg, rgba(255,255,255,1) 0%, rgba(154,154,154,1) 28%, rgba(249,249,249,1) 49%, rgba(147,147,147,1) 69%, rgba(111,111,111,1) 100%);
+            transform-origin: 60%,35% ;
+            transform:skew(.312rad) ;
+
+        } */
     </style>
     
 
 </head>
 <body> 
+        <div class="smoke"></div>
+        <div class="coffee_cup"><img src="http://pngimg.com/uploads/cup/cup_PNG1964.png" > </div>
+        <div class="m_1"></div>
+        <div class="m_2"></div>
+        <div class="hi_1">
+        </div>
+        <div class="hi_2">
+        </div>
 
-<div class="container">
+        <!-- <span class="under_word">It's all for myself to live better!</span> -->
+
+    <div class="container">
         <div class="box1">
 
         </div>
         <div class="box2">
-            <?php
+        <?php
         
         $thismonth=date("m");
         $thismonthday=date("t");
